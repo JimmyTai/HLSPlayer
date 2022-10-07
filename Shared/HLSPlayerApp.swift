1f5071b917ee8a14a131f9a1a0a0e8943b188dc9
JimmyTai <jimmy.tai@swag.live>
2022-10-07 18:05:02 +0800

    Initial Commit

---
+++ Shared/HLSPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HLSPlayerApp.swift
+//  Shared
+//
+//  Created by JimmyTai on 2022/10/7.
+//
+
+import SwiftUI
+
+@main
+struct HLSPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
